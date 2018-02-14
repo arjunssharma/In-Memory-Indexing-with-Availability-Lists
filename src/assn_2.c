584caedc1afbabf58b447b58efccf8b869fde85f
arjunssharma <32558983+arjunssharma@users.noreply.github.com>
2018-02-13 22:55:18 -0500

    added comparator for index, best and worst fit

--- src/assn_2.c
+++ src/assn_2.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- Name        : In-Memory.c
+ Name        : assn_2.c
  Author      : Arjun Sharma
  Version     :
  Copyright   : Your copyright notice
@@ -24,11 +24,48 @@ typedef struct {
 
 #define max_key_offset_pairs 10000 //Availability list will never need to store more than 10,000 size–offset pairs
 
-int worst_fit(struct avail_S *available_list, int available_list_size, int record_size) {
+int comparator_index(const void *a, const void *b);
+int comparator_ascending(const void *a, const void *b);
+int comparator_descending(const void *a, const void *b);
+int binary_search_key(struct index_S *indexes[], int key, int low, int high);
+int worst_fit(struct avail_S *available_list, int *available_list_size, int record_size);
+int best_fit(struct avail_S *available_list, int *available_list_size, int record_size);
 
-	int i = 0;
+
+int main(int argc,char* argv[]) {
+	index_S prim[500]; //index array
+    avail_S available[500]; //available list
+    FILE *fp;
+    int available_list_size= -1;
+    int index_list_size= -1;
+
+    if (argc != 3) {
+		printf("Illegal Number of Arguments");
+		exit(0);
+	}
+
+
+
+}
+
+
+
+int binary_search_key(struct index_S *prim[], int key, int low, int high) {
+	int mid = (low + high)/2;
+	if(low <= high) {
+		if(prim[mid] == key)
+			return mid;
+		else if(prim[mid] > key)
+			return binary_search(prim, key, low, mid - 1);
+		else
+			return binary_search(prim, key, mid + 1, high);
+	}
+	return -1;
+}
+
+int worst_fit(struct avail_S *available_list, int *available_list_size, int record_size) {
 	int is_available = 0;
-
+	int i = 0;
 	for (i = 0; i <= *available_list_size; i++) {
 		if (available_list[i].size >= record_size) {
 			is_available = 1;
@@ -38,21 +75,78 @@ int worst_fit(struct avail_S *available_list, int available_list_size, int recor
 				//Add hole size at the end
 				available_list[*available_list_size].size = hole_size;
 				available_list[*available_list_size].off = available_list[i].off + record_size;
-
 			}
 			//break from this loop as we found the space
 			break;
 		}
 	}
 
-	if (is_available == 1) {
-		return i;
+	if (is_available == 0) {
+		return -1; //space not found
 	} else {
-		return -1;
+		return i;
 	}
 }
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+
+int best_fit(struct avail_S *available_list, int *available_list_size, int record_size) {
+	int is_available = 0;
+	int i = 0;
+	for (i = 0; i <= *available_list_size; i++) {
+		if (available_list[i].size >= record_size) {
+			is_available = 1;
+			int hole_size = available_list[i].size - record_size;
+			if (hole_size > 0) { //append it to available list only if there is any space left
+				(*available_list_size)++;
+				//Add hole size at the end
+				available_list[*available_list_size].size = hole_size;
+				available_list[*available_list_size].off = available_list[i].off + record_size;
+			}
+			//break from this loop as we found the space
+			break;
+		}
+	}
+
+	if (is_available == 0) {
+		return -1; //space not found
+	} else {
+		return i;
+	}
+}
+
+
+int comparator_index(const void *a, const void *b)
+{
+	index_S *index_a=(index_S *) a;
+	index_S *index_b=(index_S *) b;
+	return index_a->key - index_b->key;
+
+}
+
+
+int comparator_ascending(const void *a, const void *b)
+{
+	avail_S *avail_a=(avail_S *) a;
+	avail_S *avail_b=(avail_S *) b;
+
+	int size = avail_a->size - avail_b->size;
+    if(size != 0) {
+    	return size;
+    } else {
+    	return (int)(avail_a->off - avail_b->off);
+    }
+}
+
+
+int comparator_descending(const void *a, const void *b)
+{
+	avail_S *avail_a=(avail_S *) a;
+	avail_S *avail_b=(avail_S *) b;
+
+	int size = avail_b->size - avail_a->size;
+	if(size != 0) {
+		return size;
+	} else {
+		return (int)(avail_b->off - avail_a->off);
+	}
 }
